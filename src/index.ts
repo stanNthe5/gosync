bc1a381b3469d03dd284f50a0610966d382235b6
stanNthe5 <stan@frontless.dev>
2025-03-24 02:08:01 +0800

    0.1

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+// A class that implements a Channel (like a queue) with a maximum capacity
+export class channel<T> {
+    // The queue that holds waiting items when the channel is full
+    private queue: { resolve: (v: any) => void, item: T }[] = [];
+    // The list of items currently in the channel
+    private items: T[] = [];
+    // The queue of waiting consumers (take operations) that are waiting for items to become available
+    private takeQueue: ((v: any) => void)[] = [];
+
+    // Constructor accepts the maximum size of the channel
+    constructor(private max: number) { }
+
+    // Adds an item to the channel
+    async add(item: T) {
+        // If there are consumers waiting for items, fulfill the first one
+        if (this.takeQueue.length) {
+            let resolve = this.takeQueue.shift(); // Get the first waiting consumer
+            if (resolve) {
+                resolve(item); // Fulfill the consumer's promise with the item
+            }
+        }
+
+        // If the channel is not full, add the item to the channel directly
+        if (this.items.length < this.max) {
+            this.items.push(item);
+            return;
+        } else {
+            // If the channel is full, return a promise that will resolve when space is available
+            return new Promise<void>((resolve) => this.queue.push({ resolve, item }));
+        }
+    }
+
+    // Takes an item from the channel
+    async take(): Promise<T | undefined> {
+        // If there are no items available, wait for an item to be added
+        if (!this.items.length) {
+            return new Promise<T>((resolve) => this.takeQueue.push(resolve)); // Push resolve function to takeQueue
+        }
+
+        // Retrieve the first item from the channel
+        let item = this.items.shift();
+        // If there are items in the queue, move one item to the channel
+        if (this.queue.length) {
+            let queue = this.queue.shift(); // Get the first item waiting in the queue
+            if (queue) {
+                queue.resolve(1); // Fulfill the waiting promise with some value (could be `1` as a dummy value)
+                this.items.push(queue.item); // Push the item to the channel
+            }
+        }
+
+        // Return the item that was taken
+        return item;
+    }
+
+    // Returns the current number of running tasks in the channel
+    length(): number {
+        return this.items.length;
+    }
+
+}
+
+// A class that implements a simple WaitGroup pattern, to track the completion of multiple tasks
+export class waitGroup {
+    // The number of tasks to wait for
+    private taskNum = 0;
+    // A function to resolve the promise when all tasks are done
+    private resolve?: (value: unknown) => void;
+
+    // Adds a number of tasks to wait for
+    add(n: number) {
+        this.taskNum += n;
+    }
+
+    // Marks one task as completed
+    done() {
+        this.taskNum--;
+        // If all tasks are done, resolve the promise
+        if (!this.taskNum && this.resolve) {
+            this.resolve(1);
+        }
+    }
+
+    // Returns a promise that resolves when all tasks are completed
+    all() {
+        return new Promise(resolve => {
+            this.resolve = resolve;
+        });
+    }
+}
---
+++ test/index.ts
@@ -0,0 +1,65 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { channel, waitGroup } from '../dist/index.js';
+
+test('channel FIFO add/take', async () => {
+    const ch = new channel<number>(3);
+
+    await ch.add(1);
+    await ch.add(2);
+    await ch.add(3);
+
+    assert.strictEqual(await ch.take(), 1);
+    assert.strictEqual(await ch.take(), 2);
+    assert.strictEqual(await ch.take(), 3);
+});
+
+test('waitGroup resolves after all tasks complete', async () => {
+    const wg = new waitGroup();
+    wg.add(3);
+
+    setTimeout(() => wg.done(), 100);
+    setTimeout(() => wg.done(), 200);
+    setTimeout(() => wg.done(), 300);
+
+    const start = Date.now();
+    await wg.all();
+    const end = Date.now();
+
+    assert(end - start >= 300);
+});
+
+
+test('Execute tasks with a maximum of 5 concurrent tasks', async () => {
+    const c = new channel<null>(5);
+    const wg = new waitGroup();
+
+    console.time('total');
+
+    for (let i = 1; i <= 20; i++) {
+        wg.add(1);
+        await c.add(null);
+
+        (async () => {
+            await asyncTask(i.toString());
+            await c.take();
+            wg.done();
+        })();
+    }
+
+    await wg.all();
+    console.timeEnd('total');
+
+    // check all tasks done
+    assert.strictEqual(c.length(), 0);
+});
+
+
+function asyncTask(id: string) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`finished ${id}`);
+            resolve(1);
+        }, Math.random() * 1000 + 500);
+    });
+};
